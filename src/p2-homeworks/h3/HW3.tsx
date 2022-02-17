3c40be860f125bb2f7b548d676dc9852ae4b8c1b
Natalya1984 <lisa.volma1984@gmail.com>
2022-02-18 00:59:05 +0300

    lesson1/2/3

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,27 +1,32 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
+import {v1} from "uuid";
+import s from './Greeting.module.css'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name: string // need to fix any
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
+    console.log(users)
+    const addUserCallback = (name: string) => { // need to fix any
+        setUsers([{_id: v1(), name}, ...users]) // need to fix
     }
 
+
     return (
-        <div>
+        <div className={s.container}>
             <hr/>
             homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer users={users}
+                               addUserCallback={addUserCallback}/>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
